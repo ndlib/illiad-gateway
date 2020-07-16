01ffe662825610e17626717065ca4f4db64107ba
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-07-16 16:48:15 -0400

    Implement service.

---
+++ src/web.js
@@ -0,0 +1,26 @@
+const fetch = require('node-fetch')
+const { t: typy } = require('typy')
+const { mapApiItems, requestHeaders } = require('./shared/helpers')
+const { successResponse } = require('./shared/response')
+const { sentryWrapper } = require('./shared/sentryWrapper')
+
+module.exports.handler = sentryWrapper(async (event, context, callback) => {
+  const netid = typy(event, 'requestContext.authorizer.netid').safeString
+  const filter = encodeURIComponent("(TransactionStatus eq 'Delivered to Web')")
+  const url = `${process.env.ILLIAD_URL}/${netid}?$filter=${filter}`
+  console.log('requesting url', url)
+
+  const response = await fetch(url, { headers: requestHeaders })
+    .then(res => ({
+      statusCode: res.status,
+      data: res.ok ? res.json() : null,
+    }))
+
+  if (response.statusCode < 200 || response.statusCode >= 300) {
+    // Technically use success because we handled the http error
+    return successResponse(callback, null, response.statusCode)
+  }
+
+  const results = mapApiItems(response.data)
+  return successResponse(callback, results)
+})
