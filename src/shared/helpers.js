7f65083a77b3f7990d66b1e27c1fa633b520e30c
Paul Weikel <pweikel@nd.edu>
2021-03-08 13:28:05 -0500

    changed renewalsAllowed to renewable

--- src/shared/helpers.js
+++ src/shared/helpers.js
@@ -26,7 +26,7 @@ module.exports.mapApiItems = (items) => {
       issn: item.ISSN,
       illNumber: item.ILLNumber,
       documentType: item.DocumentType,
-      renewalsAllowed: item.RenewalsAllowed,
+      renewable: item.RenewalsAllowed,
     }
 
     if (type === LOAN_TYPE) {
